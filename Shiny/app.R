e3b1292e575c0c35c5c4ffb8f9f94fa93717da66
ipiup <caro.hamery@gmail.com>
2021-05-03 17:29:53 +0200

    Upload data path choose Write fil in this path

--- Shiny/app.R
+++ Shiny/app.R
@@ -2,52 +2,63 @@ library(shiny)
 library(shinyFiles)
 
 
-fun_test<-function(x){
-  return(paste(x,"is your input"))
-  
+d=read.table("C:\\Users\\Chamery\\Documents\\Shiny_SF testshinyfile.txt")
+n="cc.txt"
+
+fun_test(d,"C:\\Users\\Chamery\\Documents\\test.txt")
+fun_test<-function(f,pth){
+  write.table(f,pth)
 }
 
 ui <- fluidPage( # Application title
   mainPanel(
     shinyDirButton("dir", "Input directory", "Upload"),
     verbatimTextOutput("dir", placeholder = TRUE),
-    textOutput("txt_out")
+    textInput("filename","Choose a file name"),
+    actionButton("namego","go"),
+    textOutput("b"),
+    actionButton("filego","write")
   ))
 
 server <- function(input, output) {
-  shinyDirChoose(
-    input,
-    'dir',
-    roots = c(wd = 'C:'),
-    filetypes = c('', 'txt')
-  )
+  shinyDirChoose(input,'dir',roots = c(wd = 'C:'),filetypes = c('', 'txt'))
   
-  global <- reactiveValues(datapath = getwd())
+  global <- reactiveValues(datapath = "")#getwd())
   
   dir <- reactive(input$dir)
+  
+  filename<-reactive(input$filename)
   
   output$dir <- renderText({
     global$datapath
   })
+  output$filename<-renderText(input$filename)
   
+  output$b<-renderText(inpu$dir)
   observeEvent(
     ignoreNULL = TRUE,
-               eventExpr = {
-                 input$dir
-               },
+               eventExpr = {input$dir},
                handlerExpr = {
                  if (!"path" %in% names(dir())) return()
-                 home <-("~")
+                 home <- normalizePath("~")
                  global$datapath <-
-                   normalizePath(file.path(home, paste(unlist(dir()$path[-1]), collapse = .Platform$file.sep)))
+                   normalizePath(file.path(home))#,paste(unlist(dir()$path[-1])), collapse = .Platform$file.sep))
                }
     )
+
   
-  output$txt_out<-renderText({global$datapath
-    #write.table("blablabla",paste(global$datapath,"testshinyfile.txt"))
-    })
-  observe(print(global$datapath))
-  
+  # output$txt_out<-renderText({global$datapath
+  #   #write.table("blablabla",paste(global$datapath,"testshinyfile.txt"))
+  #   })
+  #reac<-eventReactive(input$namego,{input$filename})
+  #output$b<-renderText({reac()})
+  #observe(print(paste0(global$datapath,"\\",input$filename,".txt")))
+  #observe(fun_test(d,global$datapath))
+  #reac<-eventReactive(input$namego,{print(paste0(global$datapath,"\\",input$filename,".txt"))})
+  reac_n<-eventReactive(input$namego,{paste0(global$datapath,"\\",input$filename,".txt")})
+  output$b<-renderText({reac_n()})
+  reac<-eventReactive(input$filego,{fun_test(d,reac_n())})
+  observe(reac())
 }
 
 # Run the application
